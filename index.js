a81ad9f783826060e7674fec9f258b0f6f71a85a
jglim997 <jglim997@gmail.com>
2021-05-06 15:58:56 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+// const text = ['websites','illustrations','pancakes'];
+// let count = 0;
+// let index = 0;
+// let currentText = '';
+// let letter = '';
+
+// (function type(){
+
+//     if(count === text.length){
+//         count = 0;
+
+
+//     }
+//     currentText = text[count];
+//     letter = currentText.slice(0,++index);
+//     document.querySelector('.typing').textContent = letter;
+//     if(letter.length === currentText.length){
+//         count++;
+//         index = 0;
+        
+//     }
+//     setTimeout(type,350);
+    
+    
+    
+
+// }());
+
+
+function newtrashtalk(){
+    document.getElementById("img1").src = "images/Group_294.svg";
+
+}
+
+function trashtalk(){
+    document.getElementById("img1").src = "images/Group_288.svg";
+
+}
+
+function newpega(){
+    document.getElementById("img2").src = "images/Group_2933.svg";
+}
+
+function pega(){
+    document.getElementById("img2").src = "images/Group_289.svg";
+
+
+}
+
+function newdou(){
+    document.getElementById("img3").src = "images/Group_2955.svg";
+}
+
+function dou(){
+    document.getElementById("img3").src = "images/Group_287.svg";
+
+
+}
+
+// function newred(){
+//     document.getElementById("img6").src = "images/more to come2.svg";
+// }
+// function red(){
+//     document.getElementById("img6").src = "images/more to come.svg";
+
+// }
+
+// function myFunction() {
+//     var element = document.body;
+//     element.classList.toggle("dark-mode");
+// }
---
+++ indexlight.js
@@ -0,0 +1,37 @@
+function newtrashtalk(){
+    document.getElementById("img1").src = "images/Group 228.svg";
+    // 204 IS ORIGINAL
+}
+
+function trashtalk(){
+    document.getElementById("img1").src = "images/trashtalkorange.svg";
+
+}
+
+function newpega(){
+    document.getElementById("img2").src = "images/Group 229.svg";
+}
+
+function pega(){
+    document.getElementById("img2").src = "images/pegaorange.svg";
+    // 194 is org
+
+}
+
+function newdou(){
+    document.getElementById("img3").src = "images/Group 231.svg";
+}
+
+function dou(){
+    document.getElementById("img3").src = "images/douorange.svg";
+    // 194 is org
+
+}
+
+// function newred(){
+//     document.getElementById("img6").src = "images/more to come2.svg";
+// }
+// function red(){
+//     document.getElementById("img6").src = "images/more to come.svg";
+
+// }
