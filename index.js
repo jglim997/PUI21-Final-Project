aef6e100dd2652dbca5370d77a5fe674da98490e
jglim997 <jglim997@gmail.com>
2021-05-07 23:43:24 -0400

    cleaning code

--- index.js
+++ index.js
@@ -1,40 +1,9 @@
-// const text = ['websites','illustrations','pancakes'];
-// let count = 0;
-// let index = 0;
-// let currentText = '';
-// let letter = '';
-
-// (function type(){
-
-//     if(count === text.length){
-//         count = 0;
-
-
-//     }
-//     currentText = text[count];
-//     letter = currentText.slice(0,++index);
-//     document.querySelector('.typing').textContent = letter;
-//     if(letter.length === currentText.length){
-//         count++;
-//         index = 0;
-        
-//     }
-//     setTimeout(type,350);
-    
-    
-    
-
-// }());
-
-
 function newtrashtalk(){
     document.getElementById("img1").src = "images/Group_294.svg";
-
 }
 
 function trashtalk(){
     document.getElementById("img1").src = "images/Group_288.svg";
-
 }
 
 function newpega(){
@@ -43,8 +12,6 @@ function newpega(){
 
 function pega(){
     document.getElementById("img2").src = "images/Group_289.svg";
-
-
 }
 
 function newdou(){
@@ -53,19 +20,4 @@ function newdou(){
 
 function dou(){
     document.getElementById("img3").src = "images/Group_287.svg";
-
-
 }
-
-// function newred(){
-//     document.getElementById("img6").src = "images/more to come2.svg";
-// }
-// function red(){
-//     document.getElementById("img6").src = "images/more to come.svg";
-
-// }
-
-// function myFunction() {
-//     var element = document.body;
-//     element.classList.toggle("dark-mode");
-// }
